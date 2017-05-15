d69a462b74780eb5ed903a95e2c6ff45ae84a3dc
louis-pham <l.c.pham@hotmail.com>
2017-05-15 13:15:45 -0400

    Update q1.py

--- question1/q1.py
+++ question1/q1.py
@@ -2,7 +2,7 @@
 
 import os
 import sys
-
+# call "python q1.py <find> <replace>" in terminal to run
 if len(sys.argv) >= 3:
 	argFind = sys.argv[1]
 	argReplace = sys.argv[2]
@@ -10,6 +10,7 @@ if len(sys.argv) >= 3:
 	print argFind
 	print argReplace
 
+	#get all text files
 	textFiles = [ f for f in os.listdir(os.getcwd()) if '.txt' in f]
 	for filename in textFiles:
 		print filename
@@ -20,6 +21,7 @@ if len(sys.argv) >= 3:
 			print "found string"
 			newContents = fileContents.replace(argFind, argReplace)
 			newFilename = "replace/" + filename
+			#create replace folder if not exist
 			if not os.path.exists(os.path.dirname(newFilename)): # original code from http://stackoverflow.com/questions/12517451/
 				try:
 					os.makedirs(os.path.dirname(newFilename))
